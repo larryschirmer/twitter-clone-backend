e819b9f44fc4b2c04f5a9cbed4e4291eec43aff7
Larry Schirmer <larryschirmer@gmail.com>
2022-01-19 17:29:47 +0000

    feat: add hash utility functions

---
+++ src/utils/hash.js
@@ -0,0 +1,29 @@
+import bcrypt from 'bcrypt';
+/**
+ * Takes string
+ *
+ * Returns hashed string
+ *
+ * @param {string} password
+ * @returns string
+ */
+const gen = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+};
+
+/**
+ * Takes raw string and hash and compares them
+ *
+ * returns true if both strings match
+ *
+ * @param {string} password
+ * @param {string} hash
+ * @returns boolean
+ */
+const compare = async (password, hash) => {
+  const result = await bcrypt.compare(password, hash);
+  return result;
+};
+
+module.exports = { gen, compare };
